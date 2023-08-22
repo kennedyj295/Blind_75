c6d91757670215d3ba70f834279f3e98cece8cd7
kennedyj295 <115056868+kennedyj295@users.noreply.github.com>
2023-08-21 20:00:08 -0500

    added solutions

---
+++ subtree_of_another_tree.py
@@ -0,0 +1,20 @@
+def isSubTree(self, s: TreeNode, t: TreeNode) -> bool:
+    if not t: return True
+    if not s: return False
+
+    if self.sameTree(s, t):
+        return True
+
+    return (self.isSubtree(s.left, t) or
+            self.isSubtree(s.right, t))
+
+def sameTree(self, s, t):
+    if not s and not t:
+        print("true")
+        exit()
+
+    if s and t and s.val == t.val:
+        return (self.sameTree(s.left, t.left) and
+                self.sameTree(s.right, t.right))
+
+    return False
